b953972e369f0b15317b81b6035db551643f665b
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2018-11-10 22:23:04 +0100

    Cleanup

--- view/src/main/java/net/chmielowski/shoppinglist/view/items/ItemsViewModel.kt
+++ view/src/main/java/net/chmielowski/shoppinglist/view/items/ItemsViewModel.kt
@@ -7,33 +7,36 @@ import net.chmielowski.shoppinglist.Item
 import net.chmielowski.shoppinglist.ReadAction
 import net.chmielowski.shoppinglist.ReadItemsParams
 import net.chmielowski.shoppinglist.WriteAction
+import java.lang.IllegalStateException
 
 class ItemsViewModel(
-    private val add: WriteAction<String>,
-    private val read: ReadAction<ReadItemsParams, List<Item>>
+    private val addItem: WriteAction<String>,
+    private val readItems: ReadAction<ReadItemsParams, List<Item>>
 ) : ViewModel() {
 
     val entering = MutableLiveData<Boolean>()
     val items = MutableLiveData<List<ItemViewModel>>()
 
-    private var newItem: String? = null
+    private var _newItem: String? = null
+    private val newItem: String
+        get() = _newItem ?: throw IllegalStateException("User has not entered a new item name.")
 
     fun onAddNew() {
         entering.value = true
     }
 
     fun onTextChange(name: String) {
-        newItem = name
+        _newItem = name
     }
 
     @SuppressLint("CheckResult")
     fun onAddingConfirmed() {
         entering.value = false
-        add(newItem!!)
-            .andThen(read(ReadItemsParams))
+        addItem(newItem)
+            .andThen(readItems(ReadItemsParams))
             .map { list -> list.map(this::toViewModel) }
             .subscribe(items::postValue)
-        newItem = null
+        _newItem = null
     }
 
     private fun toViewModel(domainModel: Item) = ItemViewModel(domainModel.id, domainModel.name)
