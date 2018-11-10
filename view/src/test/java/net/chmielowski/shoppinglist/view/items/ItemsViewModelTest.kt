1395d6bf197f8d7d6e11c621b58839e8218e2e37
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2018-11-10 20:06:49 +0100

    Test cleanup

--- view/src/test/java/net/chmielowski/shoppinglist/view/items/ItemsViewModelTest.kt
+++ view/src/test/java/net/chmielowski/shoppinglist/view/items/ItemsViewModelTest.kt
@@ -1,44 +1,51 @@
 package net.chmielowski.shoppinglist.view.items
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
 import org.hamcrest.BaseMatcher
-import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.Description
-import org.hamcrest.Matcher
 import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
-import java.util.*
 
 class ItemsViewModelTest {
 
     @get:Rule
     var rule: TestRule = InstantTaskExecutorRule()
 
+    private lateinit var model: ItemsViewModel
+
+    @Before
+    fun setUp() {
+        model = ItemsViewModel()
+    }
+
     @Test
     fun `initially empty list`() {
-        assertThat(ItemsViewModel().items.value, `is`(emptyList()))
+        model.items shouldContainItems emptyList()
     }
 
     @Test
     fun `adding element`() {
-        val model = ItemsViewModel()
-
         model.addItem("Bread")
 
-        assertThat(model.items.value, hasItemsWithNames("Bread"))
+        model.items shouldContainItems listOf("Bread")
     }
 
-    private fun hasItemsWithNames(vararg names: String): Matcher<in List<ItemViewModel>?>? {
-        return object : BaseMatcher<List<ItemViewModel>>() {
-            override fun describeTo(description: Description?) {
-                description!!.appendText(Arrays.toString(names))
-            }
-
-            override fun matches(item: Any?): Boolean {
-                return (item as List<ItemViewModel>).map { it.name } == names.toList().toList()
-            }
-        }
+    private infix fun MutableLiveData<List<ItemViewModel>>.shouldContainItems(names: List<String>) {
+        assertThat(this, SmartMatcher(names.toString()) { actual ->
+            actual.value!!.map { it.name } == names
+        })
     }
+}
+
+class SmartMatcher<T>(private val expected: String, private val match: (T) -> Boolean) : BaseMatcher<T>() {
+    override fun describeTo(description: Description?) {
+        description!!.appendText(expected)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun matches(item: Any?) = match(item as T)
 }
