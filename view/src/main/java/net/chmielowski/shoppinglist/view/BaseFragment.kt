7ded9c44aec04f188e6553543961a4fc96986eb0
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2018-11-11 18:58:31 +0100

    BaseFragment

---
+++ view/src/main/java/net/chmielowski/shoppinglist/view/BaseFragment.kt
@@ -0,0 +1,33 @@
+package net.chmielowski.shoppinglist.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import javax.inject.Inject
+import kotlin.reflect.KClass
+
+abstract class BaseFragment<VM : ViewModel, VMF : ViewModelProvider.Factory>(
+    @LayoutRes val layout: Int,
+    modelClass: KClass<VM>
+) : Fragment() {
+
+    // because property is lazy and will be evaluated after the derived class is fully initialized
+    @Suppress("LeakingThis")
+    protected val model: VM by lazy {
+        ViewModelProviders.of(this, factory).get(modelClass.java)
+    }
+
+    @Inject
+    internal lateinit var factory: VMF
+
+    final override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = inflater.inflate(layout, container, false)!!
+}
