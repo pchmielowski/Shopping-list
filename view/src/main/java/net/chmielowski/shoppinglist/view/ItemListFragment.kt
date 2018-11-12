d075d55bfbc22fdfd0718f2bff4baaef74c31af3
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2018-11-12 18:16:18 +0100

    Hide add_new_item

--- view/src/main/java/net/chmielowski/shoppinglist/view/ItemListFragment.kt
+++ view/src/main/java/net/chmielowski/shoppinglist/view/ItemListFragment.kt
@@ -24,10 +24,14 @@ class ItemListFragment : BaseFragment<ItemsViewModel, ItemsViewModel.Factory>(
 
         model.run {
             isEnteringNew.observe {
+                add_new_item.isVisibleAnimating = !it
                 entering_new_item.isVisible = it
             }
             items.observe {
                 itemsAdapter.submitList(it)
+            }
+            suggestions.observe {
+                // TODO
             }
             add_new_item.setOnClickListener {
                 onAddNew()
@@ -35,3 +39,23 @@ class ItemListFragment : BaseFragment<ItemsViewModel, ItemsViewModel.Factory>(
         }
     }
 }
+
+var View.isVisibleAnimating: Boolean
+    get() = visibility == View.VISIBLE
+    set(value) {
+        if (value) {
+            scaleX = 0.0f
+            scaleY = 0.0f
+            visibility = View.VISIBLE
+            animate()
+                .scaleX(1.0f)
+                .scaleY(1.0f)
+                .start()
+        } else {
+            animate()
+                .scaleX(0.0f)
+                .scaleY(0.0f)
+                .withEndAction { visibility = View.GONE }
+                .start()
+        }
+    }
