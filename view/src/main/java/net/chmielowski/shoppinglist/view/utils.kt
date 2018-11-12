fdc5af844ef3815e36343a7d3a61b905ad3e0b67
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2018-11-12 17:32:18 +0100

    Utils cleanup

--- view/src/main/java/net/chmielowski/shoppinglist/view/utils.kt
+++ view/src/main/java/net/chmielowski/shoppinglist/view/utils.kt
@@ -1,11 +1,17 @@
 package net.chmielowski.shoppinglist.view
 
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 fun RecyclerView.setup(fragment: Fragment, adapter: RecyclerView.Adapter<*>) {
-    val context = fragment.requireContext()
-    this.layoutManager = LinearLayoutManager(context)
+    this.layoutManager = LinearLayoutManager(fragment.requireContext())
     this.adapter = adapter
+}
+
+fun <T> LiveData<T>.observe(owner: LifecycleOwner, observer: (T) -> Unit) {
+    observe(owner, Observer<T> { observer(it) })
 }
--- view/src/main/java/net/chmielowski/shoppinglist/view/utlis.kt
+++
@@ -1,9 +0,0 @@
-package net.chmielowski.shoppinglist.view
-
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
-
-fun <T> LiveData<T>.observe(owner: LifecycleOwner, observer: (T) -> Unit) {
-    observe(owner, Observer<T> { observer(it) })
-}
