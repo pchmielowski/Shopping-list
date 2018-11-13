b858e6a05d18d618c3ca9aea8690daa590226197
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2018-11-13 22:56:18 +0100

    Divider

--- view/src/main/java/net/chmielowski/shoppinglist/view/utils.kt
+++ view/src/main/java/net/chmielowski/shoppinglist/view/utils.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 fun RecyclerView.setup(fragment: Fragment, adapter: RecyclerView.Adapter<*>) {
-    this.layoutManager = LinearLayoutManager(fragment.requireContext())
+    val manager = LinearLayoutManager(fragment.requireContext())
+    this.layoutManager = manager
     this.adapter = adapter
+    addItemDecoration(DividerItemDecoration(context, manager.orientation))
 }
 
 fun <T> LiveData<T>.observe(owner: LifecycleOwner, observer: (T) -> Unit) {
