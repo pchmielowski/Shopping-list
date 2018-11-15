9e287fb9a00b81f387bfae234ea049ec7f9db414
Piotrek Chmielowski <piotrek.chmielowski@interia.pl>
2018-11-15 15:41:26 +0100

    Navigation between fragments

--- view/src/main/java/net/chmielowski/shoppinglist/view/ShopListFragment.kt
+++ view/src/main/java/net/chmielowski/shoppinglist/view/ShopListFragment.kt
@@ -2,6 +2,7 @@ package net.chmielowski.shoppinglist.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import kotlinx.android.synthetic.main.shop_list_fragment.*
 import net.chmielowski.shoppinglist.view.shops.ShopsAdapter
@@ -18,5 +19,9 @@ class ShopListFragment : BaseFragment(R.layout.shop_list_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         shops.layoutManager = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
         shops.adapter = adapter
+
+        add_shop.setOnClickListener {
+            view.findNavController().navigate(R.id.action_shopList_to_addShop)
+        }
     }
 }
