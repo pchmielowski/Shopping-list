a22510a01ee61b2a0b98cddc834931a34e5fd499
Piotrek Chmielowski <piotrek.chmielowski@interia.pl>
2018-11-16 10:16:14 +0100

    Add shop button

--- view/src/main/java/net/chmielowski/shoppinglist/view/shops/ShopListFragment.kt
+++ view/src/main/java/net/chmielowski/shoppinglist/view/shops/ShopListFragment.kt
@@ -2,11 +2,9 @@ package net.chmielowski.shoppinglist.view.shops
 
 import android.os.Bundle
 import android.view.View
-import androidx.annotation.IdRes
-import androidx.navigation.fragment.findNavController
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.google.android.material.button.MaterialButton
 import kotlinx.android.synthetic.main.shop_list_fragment.*
 import net.chmielowski.shoppinglist.view.BaseFragment
 import net.chmielowski.shoppinglist.view.R
@@ -27,11 +25,21 @@ class ShopListFragment : BaseFragment(R.layout.shop_list_fragment) {
     private val model by getViewModel { modelFactory }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        shops.setup()
+        shop_list.setup()
+        model.run {
+            shops.bindAdapter(shopsAdapter)
+            noShops.observe { empty ->
+                no_shops.isVisible = empty
+                shop_list.isVisible = !empty
+                add_shop.isVisible = !empty
+            }
+        }
+        add_shop.onClickNavigateToAddNew()
+        add_first_shop.onClickNavigateToAddNew()
+    }
 
-        model.shops.bindAdapter(shopsAdapter)
-
-        add_shop.onClickNavigateTo(R.id.action_shopList_to_addShop)
+    private fun View.onClickNavigateToAddNew() {
+        onClickNavigateTo(R.id.action_shopList_to_addShop)
     }
 
     private fun RecyclerView.setup() {
