1f7f85636fec2e6ea8536de10c1cf36d46108636
Piotrek Chmielowski <piotrek.chmielowski@interia.pl>
2018-11-16 10:35:28 +0100

    Stetho

--- app/src/main/java/net/chmielowski/shoppinglist/CustomApplication.kt
+++ app/src/main/java/net/chmielowski/shoppinglist/CustomApplication.kt
@@ -1,12 +1,14 @@
 package net.chmielowski.shoppinglist
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import net.chmielowski.shoppinglist.view.ViewComponent
 
 class CustomApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        Stetho.initializeWithDefaults(this)
         ViewComponent.instance = DaggerAppComponent.builder()
             .bindApplicationContext(this)
             .build()
