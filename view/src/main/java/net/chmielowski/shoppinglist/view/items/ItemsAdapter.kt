d0f86d1abd57f03f135567c11142e059811fc20d
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2018-11-17 18:18:17 +0100

    Toggle checkbox on item clicked

--- view/src/main/java/net/chmielowski/shoppinglist/view/items/ItemsAdapter.kt
+++ view/src/main/java/net/chmielowski/shoppinglist/view/items/ItemsAdapter.kt
@@ -1,5 +1,6 @@
 package net.chmielowski.shoppinglist.view.items
 
+import android.annotation.SuppressLint
 import kotlinx.android.synthetic.main.item_view.*
 import net.chmielowski.shoppinglist.view.BaseListAdapter
 import net.chmielowski.shoppinglist.view.LayoutContainerViewHolder
@@ -8,9 +9,13 @@ import javax.inject.Inject
 
 class ItemsAdapter @Inject constructor() : BaseListAdapter<ItemViewModel>(R.layout.item_view) {
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: LayoutContainerViewHolder, position: Int) {
         val item = getItem(position)
         holder.item_name.text = "${item.name} ${item.quantity ?: ""}"
         holder.item_checked.isChecked = item.completed
+        holder.item_name.setOnClickListener {
+            holder.item_checked.toggle()
+        }
     }
 }
