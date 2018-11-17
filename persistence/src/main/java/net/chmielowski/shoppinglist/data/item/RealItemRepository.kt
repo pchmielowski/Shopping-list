9e1c3809276c3361268b177d0c02f1fe1a7d76a3
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2018-11-17 21:54:06 +0100

    Repository cleanup

--- persistence/src/main/java/net/chmielowski/shoppinglist/data/item/RealItemRepository.kt
+++ persistence/src/main/java/net/chmielowski/shoppinglist/data/item/RealItemRepository.kt
@@ -1,22 +1,19 @@
 package net.chmielowski.shoppinglist.data.item
 
 import dagger.Lazy
-import io.reactivex.Completable
 import net.chmielowski.shoppinglist.Id
 import net.chmielowski.shoppinglist.ItemEntity
 import net.chmielowski.shoppinglist.asSingle
 import net.chmielowski.shoppinglist.mapCompletable
 import javax.inject.Inject
 
-class RealItemRepository @Inject constructor(private val dao: Lazy<ItemDao>) :
-    ItemRepository {
+class RealItemRepository @Inject constructor(private val dao: Lazy<ItemDao>) : ItemRepository {
     override fun observeItems(completed: Boolean, shopId: Id) =
         dao.asSingle().flatMapObservable { it.findItems(completed, shopId) }!!
 
-    override fun insert(entity: ItemEntity): Completable = dao.asSingle()
-        .map { it.insert(entity) }
-        .ignoreElement()
+    override fun insert(entity: ItemEntity) = dao.asSingle()
+        .mapCompletable { it.insert(entity) }
 
-    override fun updateCompleted(id: Id, completed: Boolean): Completable =
+    override fun updateCompleted(id: Id, completed: Boolean) =
         dao.asSingle().mapCompletable { it.updateCompleted(id, completed) }
 }
