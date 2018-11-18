d0a22563ac3426565ad3be038c8eead78f887425
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2018-11-18 14:31:05 +0100

    ShopRepository removed

--- persistence/src/main/java/net/chmielowski/shoppinglist/PersistenceModule.kt
+++ persistence/src/main/java/net/chmielowski/shoppinglist/PersistenceModule.kt
@@ -5,16 +5,11 @@ import dagger.Module
 import dagger.Provides
 import net.chmielowski.shoppinglist.data.item.ItemRepository
 import net.chmielowski.shoppinglist.data.item.RealItemRepository
-import net.chmielowski.shoppinglist.data.shop.RealShopRepository
-import net.chmielowski.shoppinglist.shop.ShopRepository
 
 @Module
 abstract class PersistenceModule {
     @Binds
     abstract fun bindItemRepository(impl: RealItemRepository): ItemRepository
-
-    @Binds
-    abstract fun bindShopRepository(impl: RealShopRepository): ShopRepository
 
     @Module
     companion object {
