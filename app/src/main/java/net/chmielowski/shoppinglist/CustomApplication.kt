54d8a8452b48cd5e1fff039131ba57b0ff6f5066
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2018-11-18 19:43:15 +0100

    LeakCanary

--- app/src/main/java/net/chmielowski/shoppinglist/CustomApplication.kt
+++ app/src/main/java/net/chmielowski/shoppinglist/CustomApplication.kt
@@ -2,13 +2,20 @@ package net.chmielowski.shoppinglist
 
 import android.app.Application
 import com.facebook.stetho.Stetho
+import com.squareup.leakcanary.LeakCanary
 import net.chmielowski.shoppinglist.view.ViewComponent
 
 open class CustomApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return
+        }
+        LeakCanary.install(this)
+
         Stetho.initializeWithDefaults(this)
+
         ViewComponent.instance = DaggerAppComponent.builder()
             .bindApplicationContext(this)
             .build()
