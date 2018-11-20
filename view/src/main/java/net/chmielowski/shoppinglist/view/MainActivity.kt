4406904b6c3bc5d0819f967f8015c295ded08064
Piotrek Chmielowski <piotrek.chmielowski@interia.pl>
2018-11-20 13:07:25 +0100

    Setup StrictMode

--- view/src/main/java/net/chmielowski/shoppinglist/view/MainActivity.kt
+++ view/src/main/java/net/chmielowski/shoppinglist/view/MainActivity.kt
@@ -1,14 +1,25 @@
 package net.chmielowski.shoppinglist.view
 
 import android.os.Bundle
+import android.os.StrictMode
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupStrictMode()
         setTheme(R.style.AppTheme)
         setContentView(R.layout.main_activity)
+    }
+
+    private fun setupStrictMode() {
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyDeath()
+                .build()
+        )
     }
 
     val onBackPressedListeners = mutableSetOf<() -> Boolean>()
