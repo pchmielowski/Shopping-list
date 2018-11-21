9efc8f1c5e4ce3f6e9ed265ad5a733bf39ab742c
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2018-11-21 19:39:30 +0100

    Imports cleanup

--- persistence/src/main/java/net/chmielowski/shoppinglist/data/ActionModule.kt
+++ persistence/src/main/java/net/chmielowski/shoppinglist/data/ActionModule.kt
@@ -2,14 +2,11 @@ package net.chmielowski.shoppinglist.data
 
 import dagger.Binds
 import dagger.Module
-import net.chmielowski.shoppinglist.*
-import net.chmielowski.shoppinglist.data.item.AddItem
+import net.chmielowski.shoppinglist.DeleteType
+import net.chmielowski.shoppinglist.ItemQualifier
+import net.chmielowski.shoppinglist.ShopQualifier
 import net.chmielowski.shoppinglist.data.item.DeleteItem
-import net.chmielowski.shoppinglist.data.item.ObserveItems
-import net.chmielowski.shoppinglist.data.shop.AddShop
 import net.chmielowski.shoppinglist.data.shop.DeleteShop
-import net.chmielowski.shoppinglist.data.shop.GetShopAppearance
-import net.chmielowski.shoppinglist.data.shop.ObserveShops
 
 @Suppress("unused")
 @Module
