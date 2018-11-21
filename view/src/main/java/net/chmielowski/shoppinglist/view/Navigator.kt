46a45abe52a492d15bf85cba879c9a5e49b99d1a
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2018-11-21 18:11:26 +0100

    Inject navController

--- view/src/main/java/net/chmielowski/shoppinglist/view/Navigator.kt
+++ view/src/main/java/net/chmielowski/shoppinglist/view/Navigator.kt
@@ -2,25 +2,26 @@ package net.chmielowski.shoppinglist.view
 
 import androidx.annotation.StringRes
 import androidx.core.os.bundleOf
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
 import net.chmielowski.shoppinglist.Id
 import net.chmielowski.shoppinglist.view.shops.Strings
 import javax.inject.Inject
 
-class Navigator @Inject constructor(activity: MainActivity, val strings: Strings) {
-    private val navController = activity.findNavController(R.id.my_nav_host_fragment)
-
+class Navigator @Inject constructor(
+    private val strings: Strings,
+    private val controller: NavController
+) {
     fun toItemList(shopId: Id) {
         @Suppress("ConstantConditionIf")
         val destination = if (FeatureToggle.useNewItemListFragment)
             R.id.itemListNew
         else
             R.id.itemList
-        navController.navigate(destination, bundleOf(key(R.string.argument_shop_id) to shopId))
+        controller.navigate(destination, bundleOf(key(R.string.argument_shop_id) to shopId))
     }
 
     fun toAddShop() {
-        navController.navigate(R.id.addShop)
+        controller.navigate(R.id.addShop)
     }
 
     private fun key(@StringRes int: Int) = strings.get(int)
--- view/src/main/java/net/chmielowski/shoppinglist/view/items/NewItemListFragment.kt
+++ view/src/main/java/net/chmielowski/shoppinglist/view/items/NewItemListFragment.kt
@@ -1,5 +1,77 @@
 package net.chmielowski.shoppinglist.view.items
 
-import net.chmielowski.shoppinglist.view.R
+import android.content.res.ColorStateList
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.add_item_view_new.*
+import kotlinx.android.synthetic.main.item_list_fragment_new.*
+import net.chmielowski.shoppinglist.view.*
+import net.chmielowski.shoppinglist.view.items.ConfirmDialog.Companion.showItemConfirmDialog
+import net.chmielowski.shoppinglist.view.items.ConfirmDialog.Companion.showShopConfirmDialog
 
-class NewItemListFragment : BaseItemListFragment(R.layout.item_list_fragment_new)
+class NewItemListFragment : BaseItemListFragment(R.layout.item_list_fragment_new) {
+
+    override fun onInject(component: ActivityComponent) = component.inject(this)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        item_list.setup(this, itemsAdapter)
+        bottom_sheet.setup()
+        remove_list.setOnClickListener {
+            showShopConfirmDialog(shopId)
+        }
+
+        itemsAdapter.onDeleteListener = { itemId ->
+            showItemConfirmDialog(itemId)
+        }
+
+        addItemModel.run {
+            newItemNameError.observe {
+                new_item_name_layout.error = getString(R.string.error_empty_name)
+            }
+            addingCompleted.observe {
+                new_item_name.reset()
+                new_item_quantity.reset()
+            }
+
+            new_item_quantity.doOnTextChanged(this::onQuantityChange)
+            new_item_name.doOnTextChanged(this::onNewItemNameChange)
+            add_new.setOnClickListener { onAddingConfirmed() }
+        }
+        itemsModel.run {
+            shop.observe {
+                shop_name.text = it.name
+                shop_name.setCompoundDrawablesRelativeWithIntrinsicBounds(it.icon, 0, 0, 0)
+                it.color?.let { color ->
+                    shop_color.backgroundTintList =
+                            ColorStateList.valueOf(color)
+                }
+                shop_color.isVisible = it.colorVisible
+
+            }
+            items.bindAdapter(itemsAdapter)
+
+            toggle_shop_completed.setOnCheckedChangeListener { _, isChecked ->
+                onToggleShowCompleted(isChecked)
+            }
+            itemsAdapter.onCheckedListener = { id, completed ->
+                onToggled(id, completed)
+            }
+        }
+    }
+
+    private fun View.setup() {
+        val addNewLayout = BottomSheetBehavior.from(this)
+        onBackPressedListener = {
+            if (addNewLayout.isExpanded) {
+                addNewLayout.collapse()
+                true
+            } else false
+        }
+        onBackPressedListeners.add(onBackPressedListener)
+        label_add_new.setOnClickListener {
+            addNewLayout.toggleExpanded()
+        }
+    }
+}
