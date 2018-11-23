a1964f155c333b3aa7f9b72bbc11319922646416
Piotrek Chmielowski <piotrek.chmielowski@interia.pl>
2018-11-23 10:45:03 +0100

    Imports optimized

--- app/src/test/java/net/chmielowski/shoppinglist/ShopListScreenTest.kt
+++ app/src/test/java/net/chmielowski/shoppinglist/ShopListScreenTest.kt
@@ -2,8 +2,8 @@ package net.chmielowski.shoppinglist
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import dagger.Lazy
-import net.chmielowski.shoppinglist.data.shop.ShopDao
 import net.chmielowski.shoppinglist.data.shop.ObserveShops
+import net.chmielowski.shoppinglist.data.shop.ShopDao
 import net.chmielowski.shoppinglist.data.shop.ShopWithItemsCount
 import net.chmielowski.shoppinglist.view.shops.*
 import org.junit.Rule
--- bindinterface-compiler/src/main/java/net/chmielowski/bindinterface/BindInterfaceProcessor.java
+++ bindinterface-compiler/src/main/java/net/chmielowski/bindinterface/BindInterfaceProcessor.java
@@ -2,19 +2,7 @@ package net.chmielowski.bindinterface;
 
 import com.google.auto.service.AutoService;
 
-import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.Processor;
-import javax.annotation.processing.RoundEnvironment;
-import javax.lang.model.SourceVersion;
-import javax.lang.model.element.TypeElement;
-import java.io.IOException;
-import java.util.Set;
-
-import static java.util.Collections.singleton;
-import static javax.lang.model.SourceVersion.latestSupported;
-import static net.chmielowski.bindinterface.FileFactory.newKotlinFile;
-import static net.chmielowski.bindinterface.KputilsKt.fileWithModule;
 
 @SuppressWarnings("unused")
 @AutoService(Processor.class)
