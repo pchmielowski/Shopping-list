a036c7767518c6eefeaecf437d01ad8147e2f3ec
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2019-01-12 16:46:49 +0100

    Fix crash

--- view/src/main/java/net/chmielowski/shoppinglist/view/items/BaseItemListFragment.kt
+++ view/src/main/java/net/chmielowski/shoppinglist/view/items/BaseItemListFragment.kt
@@ -14,7 +14,7 @@ abstract class BaseItemListFragment(@LayoutRes layout: Int) : BaseFragment(layou
 
     protected val addItemModel by viewModel<AddItemViewModel>()
 
-    protected val itemsModel by viewModel<ItemsViewModel> { parametersOf(arguments!!.getInt(getString(R.string.argument_shop_id))) }
+    protected val itemsModel by viewModel<ItemsViewModel> { parametersOf(arguments!!.getLong(getString(R.string.argument_shop_id))) }
 
     protected lateinit var onBackPressedListener: () -> Boolean
 
