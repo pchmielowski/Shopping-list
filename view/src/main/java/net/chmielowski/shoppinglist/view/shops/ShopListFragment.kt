7788a85e7ae5916938737e1c209d9cf4a5a3c87c
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2019-01-13 20:18:59 +0100

    Simple state machine

--- view/src/main/java/net/chmielowski/shoppinglist/view/shops/ShopListFragment.kt
+++ view/src/main/java/net/chmielowski/shoppinglist/view/shops/ShopListFragment.kt
@@ -3,17 +3,22 @@ package net.chmielowski.shoppinglist.view.shops
 import android.os.Bundle
 import android.view.View
 import android.view.animation.OvershootInterpolator
+import androidx.annotation.IdRes
+import androidx.annotation.LayoutRes
 import androidx.constraintlayout.widget.ConstraintSet
-import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import androidx.transition.*
+import androidx.transition.ChangeBounds
+import androidx.transition.Fade
+import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
 import kotlinx.android.synthetic.main.shop_list_fragment.*
 import net.chmielowski.shoppinglist.view.BaseFragment
 import net.chmielowski.shoppinglist.view.R
 import net.chmielowski.shoppinglist.view.addshop.AddShopViewModel
 import net.chmielowski.shoppinglist.view.addshop.IconsAdapter
 import net.chmielowski.shoppinglist.view.setup
+import net.chmielowski.shoppinglist.view.shops.ShopListFragment.State.*
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -27,6 +32,13 @@ class ShopListFragment : BaseFragment(R.layout.shop_list_fragment) {
 
     private val addShopModel by viewModel<AddShopViewModel>()
 
+    enum class State {
+        DISPLAYING_SHOPS,
+        ENTERING_NEW_SHOP_NAME,
+        CHOOSING_ICON_AND_COLOR
+    }
+
+    private var state = DISPLAYING_SHOPS
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         shop_list.setup()
@@ -41,19 +53,40 @@ class ShopListFragment : BaseFragment(R.layout.shop_list_fragment) {
         shopsAdapter.onItemClickListener = { shopId ->
             navigator.toItemList(shopId)
         }
-        add_new.setOnClickListener {
-            ConstraintSet().run {
-                clone(requireContext(), R.layout.shop_list_fragment_adding)
-                applyTo(root)
-            }
-            val set = TransitionSet().apply {
-                ordering = TransitionSet.ORDERING_SEQUENTIAL
-                addTransition(Fade(Fade.IN))
-                    .addTransition(ChangeBounds())
-                interpolator = OvershootInterpolator()
-            }
-            TransitionManager.beginDelayedTransition(root, set)
+        cancel.setOnClickListener {
+            transitionTo(R.layout.shop_list_fragment)
+            state = DISPLAYING_SHOPS
         }
+        add_new.setOnClickListener {
+            when (state) {
+                DISPLAYING_SHOPS -> {
+                    transitionTo(R.layout.shop_list_fragment_adding)
+                    state = ENTERING_NEW_SHOP_NAME
+                }
+                ENTERING_NEW_SHOP_NAME -> {
+                    transitionTo(R.layout.shop_list_fragment_adding)
+                    state = CHOOSING_ICON_AND_COLOR
+                }
+                CHOOSING_ICON_AND_COLOR -> {
+                    transitionTo(R.layout.shop_list_fragment_adding)
+                    state = DISPLAYING_SHOPS
+                }
+            }
+        }
+    }
+
+    private fun transitionTo(@LayoutRes layout: Int) {
+        ConstraintSet().run {
+            clone(requireContext(), layout)
+            applyTo(root)
+        }
+        val set = TransitionSet().apply {
+            ordering = TransitionSet.ORDERING_SEQUENTIAL
+            addTransition(Fade(Fade.IN))
+                .addTransition(ChangeBounds())
+            interpolator = OvershootInterpolator()
+        }
+        TransitionManager.beginDelayedTransition(root, set)
     }
 
     private fun RecyclerView.setup() {
