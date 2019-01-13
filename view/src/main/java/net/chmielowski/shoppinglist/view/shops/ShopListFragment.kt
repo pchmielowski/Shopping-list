eaba1f74b9e24e58c44788d6c0500df5c3c1359c
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2019-01-13 16:04:29 +0100

    Adding shop partially moved to shop list fragment

--- view/src/main/java/net/chmielowski/shoppinglist/view/shops/ShopListFragment.kt
+++ view/src/main/java/net/chmielowski/shoppinglist/view/shops/ShopListFragment.kt
@@ -2,11 +2,18 @@ package net.chmielowski.shoppinglist.view.shops
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.OvershootInterpolator
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import androidx.transition.AutoTransition
+import androidx.transition.TransitionManager
 import kotlinx.android.synthetic.main.shop_list_fragment.*
 import net.chmielowski.shoppinglist.view.BaseFragment
 import net.chmielowski.shoppinglist.view.R
+import net.chmielowski.shoppinglist.view.addshop.AddShopViewModel
+import net.chmielowski.shoppinglist.view.addshop.IconsAdapter
+import net.chmielowski.shoppinglist.view.setup
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -14,18 +21,34 @@ class ShopListFragment : BaseFragment(R.layout.shop_list_fragment) {
 
     private val shopsAdapter by inject<ShopsAdapter>()
 
+    private val iconsAdapter by inject<IconsAdapter>()
+
     private val model by viewModel<ShopListViewModel>()
+
+    private val addShopModel by viewModel<AddShopViewModel>()
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         shop_list.setup()
+        choose_icon.setup(this, iconsAdapter, divider = false, orientation = RecyclerView.HORIZONTAL)
         model.run {
             shops.bindAdapter(shopsAdapter)
         }
+        addShopModel.run {
+            icons.bindAdapter(iconsAdapter)
+        }
+
         shopsAdapter.onItemClickListener = { shopId ->
             navigator.toItemList(shopId)
         }
         add_new.setOnClickListener {
-
+            ConstraintSet().run {
+                clone(requireContext(), R.layout.shop_list_fragment_adding)
+                applyTo(root)
+            }
+            TransitionManager.beginDelayedTransition(root, AutoTransition().apply {
+                interpolator = OvershootInterpolator()
+            })
         }
     }
 
