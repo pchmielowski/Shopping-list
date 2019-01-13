e922654489a2ffc3886e3f7446e749dc0ef80440
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2019-01-13 19:43:41 +0100

    Correct margin

--- view/src/main/java/net/chmielowski/shoppinglist/view/addshop/IconsAdapter.kt
+++ view/src/main/java/net/chmielowski/shoppinglist/view/addshop/IconsAdapter.kt
@@ -1,6 +1,5 @@
 package net.chmielowski.shoppinglist.view.addshop
 
-import android.util.TypedValue
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.icon_view.*
 import net.chmielowski.shoppinglist.IconId
@@ -12,13 +11,12 @@ import net.chmielowski.shoppinglist.view.R
 class IconsAdapter : BaseListAdapter<IconId, IconViewModel>(R.layout.icon_view) {
 
     override fun onBindViewHolder(holder: LayoutContainerViewHolder, position: Int) {
-        val margin = margin(holder)
         (holder.itemView.layoutParams as RecyclerView.LayoutParams).run {
             if (holder.adapterPosition == 0) {
-                marginStart = margin
+                marginStart *= 2
             }
             if (holder.adapterPosition == itemCount - 1) {
-                marginEnd = margin
+                marginEnd *= 2
             }
         }
         getItem(position).run {
@@ -26,9 +24,4 @@ class IconsAdapter : BaseListAdapter<IconId, IconViewModel>(R.layout.icon_view) 
             holder.icon.isSelected = isSelected
         }
     }
-
-    private fun margin(holder: LayoutContainerViewHolder) =
-        holder.itemView.resources.run {
-            TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, getDimension(R.dimen.margin), displayMetrics)
-        }.toInt()
 }
