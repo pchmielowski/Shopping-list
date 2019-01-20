58ef45768805154dcb8ddf74adbdc12a54db876f
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2019-01-20 21:37:51 +0100

    Cleanup

--- app/src/androidTest/java/net/chmielowski/shoppinglist/TestApplication.kt
+++ app/src/androidTest/java/net/chmielowski/shoppinglist/TestApplication.kt
@@ -1,13 +1,8 @@
 package net.chmielowski.shoppinglist
 
-import net.chmielowski.shoppinglist.view.ViewComponent
-
 class TestApplication : CustomApplication() {
+
     override fun onCreate() {
         super.onCreate()
-        ViewComponent.instance = DaggerTestAppComponent.builder()
-            .bindApplicationContext(this)
-            .build()
-            .plusTestViewComponent()
     }
 }
