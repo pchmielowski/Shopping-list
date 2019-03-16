f208917b0f55c73cc381d80fbb7a641433ace484
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2019-03-16 16:17:10 +0100

    Cleanup

--- view/src/main/java/net/chmielowski/shoppinglist/view/Navigator.kt
+++ view/src/main/java/net/chmielowski/shoppinglist/view/Navigator.kt
@@ -5,7 +5,6 @@ import androidx.core.os.bundleOf
 import net.chmielowski.shoppinglist.ShopId
 import net.chmielowski.shoppinglist.view.shops.Strings
 
-
 class Navigator(private val strings: Strings, private val activityProvider: MainActivity.Provider) {
 
     private val controller
@@ -13,10 +12,6 @@ class Navigator(private val strings: Strings, private val activityProvider: Main
 
     fun toItemList(shopId: ShopId) {
         controller.navigate(R.id.itemList, bundleOf(key(R.string.argument_shop_id) to shopId.value))
-    }
-
-    fun toAddShop() {
-        controller.navigate(R.id.addShop)
     }
 
     private fun key(@StringRes int: Int) = strings.get(int)
